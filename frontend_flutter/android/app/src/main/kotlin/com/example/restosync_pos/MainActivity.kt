f89cd2bbbb009a93089030b829dc7ba4eba02a23
MRGARUDA8 <amanvermaa888@gmail.com>
2026-07-27 17:30:00 +0530

    Delete frontend_flutter directory

--- frontend_flutter/android/app/src/main/kotlin/com/example/restosync_pos/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.restosync_pos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend_flutter/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:hacky_pizza_pos/app.dart';
-
-void main() {
-  runApp(const ProviderScope(child: HackyPizzaApp()));
-}
