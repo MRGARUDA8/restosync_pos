d350a8e65565ce293bdc402dc75415c4d59da34b
MRGARUDA8 <amanvermaa888@gmail.com>
2026-07-30 19:04:31 +0530

    Delete frontend_flutter directory

--- frontend_flutter/lib/core/constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String appName = 'RestoSync POS';
-  static const String apiBaseUrl = 'http://localhost:5000/api';
-  static const String socketUrl = 'http://localhost:5000';
-  
-  static const String tokenKey = 'auth_token';
-  static const String userKey = 'user_data';
-}
